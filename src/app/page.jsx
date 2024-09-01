1184b5200ab0d55bb47fc2ee11d718a6af77c130
Quintin Simpson <qsimps2@wgu.edu>
2024-09-01 20:52:17 +0500

    Muqadas urooj portfolio site

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,12 +20,12 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Crafting Digital Experiences, Designing Tomorrow.
+            Crafting Full Stack Experiences, Building Tomorrow.
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
+            Welcome to my Full Stack canvas, where innovation 
+            With a keen eye for aesthetics and a mastery of code, my
             portfolio showcases a diverse collection of projects that reflect my
             commitment to excellence.
           </p>
@@ -34,7 +34,10 @@ const Homepage = () => {
             <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
               View My Work
             </button>
-            <button className="p-4 rounded-lg ring-1 ring-black">
+            <button
+              className="p-4 rounded-lg ring-1 ring-black"
+              onClick={() => window.open('mailto:muqadasurooj123@gmail.com')}
+            >
               Contact Me
             </button>
           </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -82,31 +82,19 @@ const Navbar = () => {
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
         >
-          <span className="text-white mr-1">Lama</span>
+          <span className="text-white mr-1">Muqadas</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
-            .dev
+            Urooj
           </span>
         </Link>
       </div>
       {/* SOCIAL */}
       <div className="hidden md:flex gap-4 w-1/3">
-        <Link href="/">
-          <Image src="/github.png" alt="" width={24} height={24} />
+        <Link href="https://github.com/muqadasurooj17" passHref>
+          <Image src="/github.png" alt="GitHub" width={24} height={24} />
         </Link>
-        <Link href="/">
-          <Image src="/dribbble.png" alt="" width={24} height={24} />
-        </Link>
-        <Link href="/">
-          <Image src="/instagram.png" alt="" width={24} height={24} />
-        </Link>
-        <Link href="/">
-          <Image src="/facebook.png" alt="" width={24} height={24} />
-        </Link>
-        <Link href="/">
-          <Image src="/pinterest.png" alt="" width={24} height={24} />
-        </Link>
-        <Link href="/">
-          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        <Link href="https://www.linkedin.com/in/muqadas-urooj/" passHref>
+          <Image src="/linkedin.png" alt="LinkedIn" width={24} height={24} />
         </Link>
       </div>
       {/* RESPONSIVE MENU */}
